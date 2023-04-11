ae50b748975a2dc948b7a2e9bdd2ec1ffecc1eff
SRJ-91 <Sawyerjen@hotmail.com>
2023-04-11 12:33:03 -0500

    question 8 complete

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -65,10 +65,9 @@ function find(value, array) {
     if (array[i] === value) {
       return true;
     }
+    return false;
   }
-  return false;
 }
-
 
 console.log(find(3, [1, 2, 3, 4, 5,]));
 
@@ -79,8 +78,13 @@ console.log(find(3, [1, 2, 3, 4, 5,]));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string.charAt(0))
+    return true;
+  else {
+    return false;
+  }
 }
+
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
